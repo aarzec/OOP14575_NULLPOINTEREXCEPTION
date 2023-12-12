a6ac93c05d1a4dd2f64875d29afd2c37cd6c8f73
aarzec <andresrz2k21@gmail.com>
2023-12-11 21:11:29 -0500

    Fixed NetBeans project folders

--- 06-Code/DPEXSystem/src/ec/edu/espe/dpexsystem/view/DPEXSystem.java
+++ 06-Code/DPEXSystem/src/ec/edu/espe/dpexsystem/view/DPEXSystem.java
@@ -20,8 +20,7 @@ import java.util.Scanner;
  * @author NullPointerException
  */
 public class DPEXSystem {
-<<<<<<< HEAD
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         Country country = Constituency.addCountry();
@@ -29,17 +28,14 @@ public static void main(String[] args) {
         PackageType packageType = createPackageType();
 
         createPackage(country, consularOffice, packageType);
-=======
-    public static void main(String[] args) {
         User user = new User();
         DPEXSystem mainSystem = new DPEXSystem();
         mainSystem.adminInterface();
->>>>>>> dffe7b0d1f63bf05394b10d095252a2c643def2d
     }
 
     public static void createPackage(Country country, ConsularOffice consularOffice, PackageType packageType) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Enter the package id: ");
         int packageId = scanner.nextInt();
         scanner.nextLine();
@@ -62,69 +58,65 @@ public static void main(String[] args) {
         System.out.print("Enter the id of the package type: ");
         int id = scanner.nextInt();
         scanner.nextLine();
-        
-        //if (ecuadorianPopulation <= 100) {
-        //    type = "Cne";
-        //} else if (ecuadorianPopulation > 100 && ecuadorianPopulation < 900) {
-        //    type = "Mixto";
-        //} else {
-        //   type = "Gender";
-        //}
-        
-        System.out.print("Enter the type of the package: "); //le pido ingreso solo para que el porgrama corra 
+
+        // if (ecuadorianPopulation <= 100) {
+        // type = "Cne";
+        // } else if (ecuadorianPopulation > 100 && ecuadorianPopulation < 900) {
+        // type = "Mixto";
+        // } else {
+        // type = "Gender";
+        // }
+
+        System.out.print("Enter the type of the package: "); // le pido ingreso solo para que el porgrama corra
         String type = scanner.nextLine();
 
         return new PackageType(id, type);
     }
 
-    
-<<<<<<< HEAD
-    public static void saveToJson(Package packages) {
-=======
-    public void adminInterface(){
+    public void adminInterface() {
         Scanner scanner = new Scanner(System.in);
-        
-        while(true){
-        System.out.println("\t Sample text");
-        System.out.println("You have logged in as an Administrator" );
-        System.out.println("Please, select an option");
-        System.out.println("\t1. Register a new electoral package");
-        System.out.println("\t2. Modify a registered package");
-        System.out.println("\t3. Assign new roles");
-        System.out.println("\t4. Export electoral packages data");
-        System.out.println("\t5. Log in as a different user");
-        System.out.println("\t6. Exit system");
-        System.out.println("\n Your option: ");
-        
-        int menuOption = scanner.nextInt(); 
-        
+
+        while (true) {
+            System.out.println("\t Sample text");
+            System.out.println("You have logged in as an Administrator");
+            System.out.println("Please, select an option");
+            System.out.println("\t1. Register a new electoral package");
+            System.out.println("\t2. Modify a registered package");
+            System.out.println("\t3. Assign new roles");
+            System.out.println("\t4. Export electoral packages data");
+            System.out.println("\t5. Log in as a different user");
+            System.out.println("\t6. Exit system");
+            System.out.println("\n Your option: ");
+
+            int menuOption = scanner.nextInt();
+
             switch (menuOption) {
                 case 1:
                     registerElectoralPackage();
-                break;
+                    break;
                 case 2:
                     modifyPackages();
-                break;
+                    break;
                 case 3:
                     createNewRole();
-                break;
+                    break;
                 case 4:
-                    //saveToJson();
-                break;
+                    // saveToJson();
+                    break;
                 case 5:
                     logInNewUser();
-                break;
+                    break;
                 case 6:
                     System.out.println("Thanks for using ____");
                     System.exit(0);
-                break;
-                
+                    break;
+
                 default:
                     System.out.println("\t Invalid option. Please choose a valid option. \n");
             }
         }
     }
-    
+
     private void registerElectoralPackage() {
 
     }
@@ -138,7 +130,6 @@ public static void main(String[] args) {
     }
 
     public static void saveToJson(Package[] packages) {
->>>>>>> dffe7b0d1f63bf05394b10d095252a2c643def2d
         Gson gson = new Gson();
         ArrayList<Package> packageList = new ArrayList<>();
 
@@ -147,15 +138,12 @@ public static void main(String[] args) {
             System.out.println("Chickens saved to JSON successfully.");
         } catch (IOException e) {
             System.out.println("Error writing to JSON file: " + e.getMessage());
-          }
+        }
     }
-    
+
     private void logInNewUser() {
 
         System.out.println("Executing option 5: Log in as a different user");
     }
-    
-}
-    
-   
 
+}
