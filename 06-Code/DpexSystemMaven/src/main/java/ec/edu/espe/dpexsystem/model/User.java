d1b9da96979127ebb9eb59fa68a12e9cd0d9bf7c
Mateo Rogerón <materoge32@gmail.com>
2024-01-30 23:19:56 -0500

    DpexSystemMaven

--- 06-Code/DpexSystemMaven/src/main/java/ec/edu/espe/dpexsystem/model/User.java
+++ 06-Code/DpexSystemMaven/src/main/java/ec/edu/espe/dpexsystem/model/User.java
@@ -67,6 +67,19 @@ public class User {
         }
     }
 
+    public User(UUID id, String username, String password, UserType type, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.type = type;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public UUID getId() {
+        return id;
+    }
+
     private static String encryptPassword(String source) {
         StringBuilder encrypted = new StringBuilder();
         for (char c : source.toCharArray()) {
@@ -94,6 +107,31 @@ public class User {
         }
         return null;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
 
     public UserType getType() {
         return type;
